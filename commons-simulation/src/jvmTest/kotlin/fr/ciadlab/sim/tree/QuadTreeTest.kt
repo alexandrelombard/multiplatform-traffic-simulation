2692ea72b1a21a13d1e56361ed575a7cbcf9e899
Alexandre Lombard <alexandre.lombard@utbm.fr>
2020-12-17 15:58:30 +0100

    Working on quad-tree tests

--- commons-simulation/src/jvmTest/kotlin/fr/ciadlab/sim/tree/QuadTreeTest.kt
+++ commons-simulation/src/jvmTest/kotlin/fr/ciadlab/sim/tree/QuadTreeTest.kt
@@ -4,6 +4,7 @@ import fr.ciadlab.sim.AxisAlignedBoundingBox2D
 import fr.ciadlab.sim.math.geometry.Vector2D
 import kotlin.random.Random
 import kotlin.test.Test
+import kotlin.test.assertTrue
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
@@ -16,7 +17,7 @@ class QuadTreeTest {
         }
 
         val duration = measureTime {
-            for (i in 0..1000000) {
+            for (i in 0..10000) {
                 quadTree.add(
                     Vector2D(
                         Random.nextDouble(-100.0, 100.0),
@@ -25,6 +26,10 @@ class QuadTreeTest {
                 )
             }
         }
+
+        // Even on a potato PC, it should be below 1s for 10000 add
+        // For reference, I get 32.4ms
+        assertTrue { duration.inSeconds <= 1.0f }
 
         println("Duration: $duration - Leaves count: ${quadTree.countLeaves()}")
     }

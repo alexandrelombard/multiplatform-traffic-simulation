2e54267ddb75b1513e2719b0fa2129d2fd173b49
Alexandre Lombard <alexandre.lombard@utbm.fr>
2020-12-18 14:56:40 +0100

    Testing the method to fetch the elements of the quad tree inside a bounding box

--- commons-simulation/src/jvmTest/kotlin/fr/ciadlab/sim/tree/QuadTreeTest.kt
+++ commons-simulation/src/jvmTest/kotlin/fr/ciadlab/sim/tree/QuadTreeTest.kt
@@ -21,14 +21,42 @@ class QuadTreeTest {
                 quadTree.add(
                     Vector2D(
                         Random.nextDouble(-100.0, 100.0),
-                        Random.nextDouble(-100.0, 100.0)
-                    )
-                )
+                        Random.nextDouble(-100.0, 100.0)))
             }
         }
 
         // Even on a potato PC, it should be below 1s for 10000 add
         // For reference, I get 32.4ms
+        assertTrue { duration.inSeconds <= 1.0f }
+
+        println("Duration: $duration - Leaves count: ${quadTree.countLeaves()}")
+    }
+
+    @ExperimentalTime
+    @Test
+    fun testFetchElementsPerformance() {
+        val quadTree = QuadTree<Vector2D>(AxisAlignedBoundingBox2D(Vector2D(0.0, 0.0), 100.0)) {
+            it
+        }
+
+        for (i in 0..10000) {
+            quadTree.add(
+                Vector2D(
+                    Random.nextDouble(-100.0, 100.0),
+                    Random.nextDouble(-100.0, 100.0)))
+        }
+
+        val duration = measureTime {
+            for (i in 0..100) {
+                quadTree.fetchElements(
+                    AxisAlignedBoundingBox2D(
+                        Vector2D(Random.nextDouble(-100.0, 100.0), Random.nextDouble(-100.0, 100.0)),
+                        Random.nextDouble(10.0, 50.0)))
+            }
+        }
+
+        // Even on a potato PC, it should be below 1s for 10000 add
+        // For reference, I get 61.7ms
         assertTrue { duration.inSeconds <= 1.0f }
 
         println("Duration: $duration - Leaves count: ${quadTree.countLeaves()}")

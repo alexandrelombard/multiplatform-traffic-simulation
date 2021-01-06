59cd5d19f212f7dc6ab10184a80e6dc26f8a18e5
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-01-06 15:08:06 +0100

    Working on routing

--- car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/routing/OriginDestinationRouter.kt
+++ car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/routing/OriginDestinationRouter.kt
@@ -1,5 +1,6 @@
 package fr.ciadlab.sim.car.behavior.routing
 
+import fr.ciadlab.sim.ai.DijkstraAlgorithm
 import fr.ciadlab.sim.car.perception.mapmatching.MapMatchingProvider
 import fr.ciadlab.sim.infrastructure.Road
 import fr.ciadlab.sim.infrastructure.RoadNetwork
@@ -11,15 +12,22 @@ class OriginDestinationRouter(val roadNetwork: RoadNetwork, val mapMatchingProvi
      * Finds the route between two points
      * @param origin the origin
      * @param destination the destination
-     * @return the list of roads
+     * @return the list of roads or <code>null</code> if unable to join the destination from the origin
      */
-    fun findRoute(origin: Vector2D, destination: Vector2D): List<Road> {
+    fun findRoute(origin: Vector2D, destination: Vector2D): List<Road>? {
         val originMapMatching = mapMatchingProvider.mapMatching(origin)
         val destinationMapMatching = mapMatchingProvider.mapMatching(destination)
 
-        // TODO Use a shortest path algorithm
+        val dijkstraAlgorithm = DijkstraAlgorithm<Road>()
 
-        return arrayListOf()
+        val path = dijkstraAlgorithm.findShortestPath(
+            originMapMatching.road,
+            destinationMapMatching.road,
+            availableNodes = { roadNetwork.getConnectedRoads(it) },
+            distance = { roadA, _ -> roadA.length } // FIXME Bad metric for distance
+        )
+
+        return path
     }
 
 }

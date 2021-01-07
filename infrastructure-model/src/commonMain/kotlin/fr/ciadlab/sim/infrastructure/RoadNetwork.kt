00a3f3c9a2b4a966376d409c39c8489569af62c0
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-01-07 15:01:39 +0100

    Updating network model: the bug is caused by the cars not knowing the direction of the road to follow

--- infrastructure-model/src/commonMain/kotlin/fr/ciadlab/sim/infrastructure/RoadNetwork.kt
+++ infrastructure-model/src/commonMain/kotlin/fr/ciadlab/sim/infrastructure/RoadNetwork.kt
@@ -5,11 +5,14 @@ data class RoadNetwork(
     val intersections: List<Intersection> = arrayListOf(),
     val trafficType: TrafficType = TrafficType.RIGHT_HAND
 ) {
+    /** Contains the links between roads, allows to know which roads are connected to a given one */
     private val connections = hashMapOf<Road, MutableSet<Road>>()
+    /** Contains the links between roads and intersection, allows to know which intersections are connected to a road */
+    private val roadIntersections = hashMapOf<Road, MutableSet<Intersection>>()
 
     init {
-        // Register the connections (note: the road network is immutable)
         intersections.flatMap { it.laneConnectors }.forEach {
+            // Register the connections (note: the road network is immutable)
             val connectedRoads = connections[it.sourceRoad]
 
             if(connectedRoads == null) {
@@ -18,15 +21,37 @@ data class RoadNetwork(
                 connectedRoads.add(it.destinationRoad)
             }
         }
+
+        intersections.forEach { intersection ->
+            intersection.laneConnectors.forEach { laneConnector ->
+                // Register the link between roads and intersections
+                val connectedIntersections = roadIntersections[laneConnector.sourceRoad]
+
+                if(connectedIntersections == null) {
+                    roadIntersections[laneConnector.sourceRoad] = hashSetOf(intersection)
+                } else {
+                    connectedIntersections.add(intersection)
+                }
+            }
+        }
     }
 
     /**
      * Gets the roads connected to the given one through an intersection
      * @param road the road
-     * @return the list of roads connected to the given one
+     * @return the collection of roads connected to the given one (an empty collection if the road is not known)
      */
     fun getConnectedRoads(road: Road): Collection<Road> {
         return connections[road] ?: hashSetOf()
+    }
+
+    /**
+     * Gets the intersections connected to the given road
+     * @param road the road
+     * @return the collection of intersections connected to the road (an empty collection if the road is not known)
+     */
+    fun getIntersections(road: Road): Collection<Intersection> {
+        return roadIntersections[road] ?: hashSetOf()
     }
 }
 

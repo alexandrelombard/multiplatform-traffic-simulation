d48085a228aa37c382c8e6f74a0cf8aa265af853
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-03-08 15:36:31 +0100

    Working on intersections

--- infrastructure-model/src/commonMain/kotlin/fr/ciadlab/sim/infrastructure/intersection/TrafficLightIntersectionManager.kt
+++ infrastructure-model/src/commonMain/kotlin/fr/ciadlab/sim/infrastructure/intersection/TrafficLightIntersectionManager.kt
@@ -4,12 +4,31 @@ import fr.ciadlab.sim.infrastructure.Intersection
 import fr.ciadlab.sim.infrastructure.LaneConnector
 
 /**
- * Describes the traffic light organization over a given intersection by binding a traffic light state to a
- * lane connector
+ * Describes the traffic light organization over a given intersection by binding traffic light states to
+ * lane connectors
  * @author Alexandre Lombard
  */
-class TrafficLightIntersectionManager(
+data class TrafficLightIntersectionManager(
     val intersection: Intersection,
     val trafficLights: Map<LaneConnector, TrafficLightState>) {
 
+    /**
+     * Change the state of the given connector
+     * @param connector the lane connector
+     * @param newState the new state
+     * @return the updated traffic manager
+     */
+    fun changeState(connector: LaneConnector, newState: TrafficLightState): TrafficLightIntersectionManager {
+        return this.copy(trafficLights = this.trafficLights + (Pair(connector, newState)))
+    }
+
+    /**
+     * Change the state of the given connectors
+     * @param connectors the lane connectors
+     * @param newState the new state
+     * @return the updated traffic manager
+     */
+    fun changeState(connectors: List<LaneConnector>, newState: TrafficLightState): TrafficLightIntersectionManager {
+        return this.copy(trafficLights = this.trafficLights + connectors.map { Pair(it, newState) })
+    }
 }

7bb829a6e13b51864f9c04d55a4988e9a1104b33
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-03-18 11:05:03 +0100

    Fixing display issues with vehicle

--- view-js/src/main/kotlin/fr/ciadlab/sim/infrastructure/viewjs/vehicle/VehicleView.kt
+++ view-js/src/main/kotlin/fr/ciadlab/sim/infrastructure/viewjs/vehicle/VehicleView.kt
@@ -1,7 +1,6 @@
 package fr.ciadlab.sim.infrastructure.viewjs.car
 
 import fr.ciadlab.sim.infrastructure.viewjs.canvas.image
-import fr.ciadlab.sim.infrastructure.viewjs.canvas.line
 import fr.ciadlab.sim.vehicle.Vehicle
 import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.Image
@@ -10,15 +9,13 @@ import kotlin.math.PI
 private var carImage: Image? = null
 
 //private val imageBasePath = "assets/js/sim-view-js"
-private val imageBasePath = "build/distributions/fr/ciadlab/sim/infrastructure/viewsjs/car"
+private val imageBasePath = "./build/distributions/fr/ciadlab/sim/infrastructure/viewjs/car"
 
 class VehicleView (var vehicle: Vehicle) {
-    val image: Image
     val fitWidth: Double
     val fitHeight: Double
 
     init {
-        image = image("$imageBasePath/car_up_right.png")
         fitWidth = vehicle.length
         fitHeight = 0.5 * vehicle.length
     }
@@ -28,17 +25,9 @@ class VehicleView (var vehicle: Vehicle) {
     }
 
     fun draw(canvas: CanvasRenderingContext2D) {
-        console.log("Drawing vehicle: ${vehicle.length}")
-
         canvas.save()
         canvas.translate(vehicle.position.x, vehicle.position.y)
         canvas.rotate(vehicle.direction.alpha)
-        canvas.line {
-            startX = 0.0
-            startY = 0.0
-            endX = 5.0
-            endY = 0.0
-        }
         val carLength = vehicle.length
         val carWidth = vehicle.length / 2.0
         canvas.drawImage(
@@ -48,6 +37,10 @@ class VehicleView (var vehicle: Vehicle) {
             carLength,
             carWidth)
         canvas.restore()
+    }
+
+    companion object {
+        val image: Image = image("$imageBasePath/car_up_right.png")
     }
 
     private fun toDegrees(radian: Double) = radian / PI * 180.0

60772e2b6edbbae30ee7b39b8d5f0e78c8ef3012
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-03-19 17:13:28 +0100

    Working on Transparent Intersection Manager

--- traffic-simulation/src/commonMain/kotlin/fr/ciadlab/sim/traffic/basics/BehaviorBasics.kt
+++ traffic-simulation/src/commonMain/kotlin/fr/ciadlab/sim/traffic/basics/BehaviorBasics.kt
@@ -5,9 +5,11 @@ import fr.ciadlab.sim.car.behavior.DriverBehavioralState
 import fr.ciadlab.sim.car.behavior.default.reachGoalBehavior
 import fr.ciadlab.sim.car.behavior.default.respectTrafficLightBehavior
 import fr.ciadlab.sim.car.perception.mapmatching.MapMatchingProvider
+import fr.ciadlab.sim.car.perception.obstacles.ObstacleData
 import fr.ciadlab.sim.car.perception.obstacles.RadarPerceptionProvider
 import fr.ciadlab.sim.car.perception.signals.TrafficLightPerceptionProvider
 import fr.ciadlab.sim.infrastructure.Road
+import fr.ciadlab.sim.infrastructure.intersection.IntersectionTrafficLight
 import fr.ciadlab.sim.physics.Units
 import fr.ciadlab.sim.physics.unit
 import fr.ciadlab.sim.traffic.TrafficSimulation
@@ -17,6 +19,25 @@ import fr.ciadlab.sim.vehicle.Vehicle
  * Simple functions that can be used as default behaviors.
  * @author Alexandre Lombard
  */
+
+/**
+ * Computes the obstacle data
+ */
+fun TrafficSimulation<Vehicle>.generateObstaclePerceptions(vehicle: Vehicle): List<ObstacleData> {
+    val radar = RadarPerceptionProvider()
+    val radarData = radar.performRadarDetection(vehicle.position, vehicle.direction, this.vehicles)
+    return radarData
+}
+
+/**
+ * Computes the traffic light perception
+ */
+fun TrafficSimulation<Vehicle>.generateTrafficLightPerceptions(route: List<Pair<Road, Boolean>>?, vehicle: Vehicle) : List<IntersectionTrafficLight> {
+    val trafficLights = TrafficLightPerceptionProvider()
+    val perceivedTrafficLights = trafficLights.performTrafficLightDetection(
+        route?.map { it.first } ?: emptyList(), this.roadNetwork.trafficLights.flatMap { it.trafficLights })
+    return perceivedTrafficLights
+}
 
 /**
  * Basic vehicle behavior
@@ -31,23 +52,20 @@ fun TrafficSimulation<Vehicle>.basicVehicleBehavior (
     val mapPosition = mapMatcher.mapMatching(vehicle.position)
     val forward = route?.find { it.first == mapPosition.road }?.second
 
-    // Compute perceptions
-    val radar = RadarPerceptionProvider()
-    val radarData = radar.performRadarDetection(vehicle.position, vehicle.direction, this.vehicles)
-    val trafficLights = TrafficLightPerceptionProvider()
-    val perceivedTrafficLights = trafficLights.performTrafficLightDetection(
-        route?.map { it.first } ?: emptyList(), this.roadNetwork.trafficLights.flatMap { it.trafficLights })
+    // Compute perceptions   // TODO Externalize perceptions
+    val obstacleData = generateObstaclePerceptions(vehicle)
+    val trafficLights = generateTrafficLightPerceptions(route, vehicle)
 
     // Execute the behavior
     val driverBehavioralState = DriverBehavioralState(
         mapPosition.road,
         0,      // FIXME
         forward ?: true,
-        radarData,
+        obstacleData,
         50.0 unit Units.KilometersPerHour,
         route?.last()?.first?.end() ?: this.roadNetwork.roads[0].end())
 
     return vehicle.reachGoalBehavior(driverBehavioralState)
-        .and(vehicle.respectTrafficLightBehavior(driverBehavioralState, perceivedTrafficLights))
+        .and(vehicle.respectTrafficLightBehavior(driverBehavioralState, trafficLights))
         .apply(deltaTime)
 }

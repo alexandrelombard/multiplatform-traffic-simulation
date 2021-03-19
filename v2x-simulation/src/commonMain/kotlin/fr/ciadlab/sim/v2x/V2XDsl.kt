498c94e080f37e284c4174b292b942a83907036f
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-03-19 08:39:13 +0100

    Refactor

--- v2x-simulation/src/commonMain/kotlin/fr/ciadlab/sim/v2x/V2XDsl.kt
+++ v2x-simulation/src/commonMain/kotlin/fr/ciadlab/sim/v2x/V2XDsl.kt
@@ -1,14 +1,11 @@
-package fr.ciadlab.sim.v2x.intersection
-
-import fr.ciadlab.sim.infrastructure.DslIntersection
-import fr.ciadlab.sim.v2x.V2XCommunicationUnit
+package fr.ciadlab.sim.v2x
 
 data class RoadSideUnitDsl(
     var protocol: (deltaTime: Double) -> Unit = {},
     var communicationUnit: V2XCommunicationUnit = V2XCommunicationUnit()
 )
 
-fun DslIntersection.roadSideUnit(op: RoadSideUnitDsl.() -> Unit): RoadSideUnitDsl {
+fun roadSideUnit(op: RoadSideUnitDsl.() -> Unit): RoadSideUnitDsl {
     val roadSideUnitDsl = RoadSideUnitDsl()
     op.invoke(roadSideUnitDsl)
 

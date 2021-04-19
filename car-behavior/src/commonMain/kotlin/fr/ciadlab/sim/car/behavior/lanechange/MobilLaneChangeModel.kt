26704e6b81a0f2620eff8db8720f749f7b503904
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-04-19 11:59:42 +0200

    Working on lane-change model

--- car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/lanechange/MobilLaneChangeModel.kt
+++ car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/lanechange/MobilLaneChangeModel.kt
@@ -1,2 +1,38 @@
 package fr.ciadlab.sim.car.behavior.lanechange
 
+import fr.ciadlab.sim.car.perception.obstacles.ObstacleData
+
+/**
+ * MOBIL-like lane change model
+ * @param currentSpeed
+ * @param freeSpeed
+ * @param currentLeader the current leader
+ * @param targetLanePerceivedVehicles the perceived vehicles in the target lane
+ * @param curryiedCarFollowingModel a car-following model related to the self vehicle
+ */
+fun mobilLaneChange(
+    currentSpeed: Double,
+    freeSpeed: Double,
+    currentLeader: ObstacleData,
+    targetLanePerceivedVehicles: List<ObstacleData>,
+    curryiedCarFollowingModel: (ObstacleData)->Double) {
+
+    if(currentSpeed < freeSpeed) {
+        // If the current speed is below the free speed, and if we can accelerate in the new lane without making someone
+        // brake, we do it
+        val currentLeaderRelatedAcceleration = curryiedCarFollowingModel(currentLeader)
+
+        val newLeader = targetLanePerceivedVehicles
+            .filter { it.obstacleRelativePosition.x >= 0 }.minByOrNull { it.obstacleRelativePosition.x }
+        val newFollower = targetLanePerceivedVehicles
+            .filter { it.obstacleRelativePosition.x < 0 }.maxByOrNull { it.obstacleRelativePosition.x }
+
+        val newLeaderRelatedAcceleration =
+            if(newLeader == null) Double.POSITIVE_INFINITY else curryiedCarFollowingModel(newLeader)
+
+        if(newLeaderRelatedAcceleration > currentLeaderRelatedAcceleration) {
+
+        }
+    }
+
+}

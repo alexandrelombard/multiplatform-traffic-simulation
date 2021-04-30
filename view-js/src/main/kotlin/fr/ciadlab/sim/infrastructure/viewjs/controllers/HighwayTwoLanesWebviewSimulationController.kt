f201e45501c785acdfa1dd19d313ad2beeaa75a4
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-04-30 09:18:05 +0200

    Preparing build for view JS

---
+++ view-js/src/main/kotlin/fr/ciadlab/sim/infrastructure/viewjs/controllers/HighwayTwoLanesWebviewSimulationController.kt
@@ -0,0 +1,41 @@
+package fr.ciadlab.sim.infrastructure.viewjs.controllers
+
+import fr.ciadlab.sim.infrastructure.viewjs.canvas.clear
+import fr.ciadlab.sim.infrastructure.viewjs.generateBaseCanvas
+import fr.ciadlab.sim.infrastructure.viewjs.simulation.trafficSimulationView
+import fr.ciadlab.sim.physics.Units
+import fr.ciadlab.sim.physics.unit
+import fr.ciadlab.sim.traffic.scenario.HighwaySection2Lanes
+import kotlinx.browser.window
+import org.w3c.dom.CanvasRenderingContext2D
+
+class HighwayTwoLanesWebviewSimulationController {
+    private val simulationStepTime = 10.0 unit Units.Milliseconds
+
+    // region Parameters
+    // endregion
+
+    /**
+     * Loads the simulation view in the given canvas
+     */
+    @JsName("load")
+    fun load(canvasId: String) {
+        val canvas = generateBaseCanvas(arrayOf(canvasId))
+        val context = canvas.getContext("2d") as CanvasRenderingContext2D
+
+        val roadNetworkModel = HighwaySection2Lanes.network
+        val simulation = HighwaySection2Lanes.simulation
+
+        // Drawing loop
+        window.setInterval({
+            context.clear(canvas)
+
+            context.trafficSimulationView(simulation)
+        }, 20)
+
+        // Simulation loop
+        window.setInterval({
+            simulation.step(simulationStepTime)
+        }, 10)
+    }
+}

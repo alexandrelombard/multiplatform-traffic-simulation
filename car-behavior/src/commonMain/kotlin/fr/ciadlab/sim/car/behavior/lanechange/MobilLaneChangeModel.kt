4b49daebaeea67c82a19dbd22d365e93b20d4ffe
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-05-06 08:32:47 +0200

    Removing unused mobilLaneChange function

--- car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/lanechange/MobilLaneChangeModel.kt
+++ car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/lanechange/MobilLaneChangeModel.kt
@@ -1,8 +1,5 @@
 package fr.ciadlab.sim.car.behavior.lanechange
 
-import fr.ciadlab.sim.car.perception.obstacles.ObstacleData
-import kotlin.math.abs
-
 /**
  * distance is the distance between vehicles
  * relativeSpeed is the relative speed between the vehicles (follower and leader)
@@ -75,54 +72,5 @@ data class MobilState(
         // TODO We should compare the expected acceleration gain to the imposed acceleration loss of the new follower (https://traffic-simulation.de/info/info_MOBIL.html)
         return isLaneChangeSafe(carFollowingModel, decelerationThreshold) && isLaneChangeProfitable(carFollowingModel, accelerationThreshold)
     }
-
-}
-
-/**
- * MOBIL-like lane change model
- * @param currentSpeed
- * @param freeSpeed
- * @param currentLeader the current leader
- * @param targetLanePerceivedVehicles the perceived vehicles in the target lane
- * @param curryiedCarFollowingModel a car-following model related to the self vehicle
- * @return either <code>true</code> or <code>false</code> if the lane change should be performed or not
- */
-fun mobilLaneChange(
-    currentSpeed: Double,
-    freeSpeed: Double,
-    currentLeader: ObstacleData?,
-    targetLanePerceivedVehicles: List<ObstacleData>,
-    curryiedCarFollowingModel: (ObstacleData)->Double): Boolean {
-
-    if(currentSpeed < freeSpeed) {
-        // If the current speed is below the free speed, and if we can accelerate in the new lane without making someone
-        // brake, we do it
-        val currentLeaderRelatedAcceleration =
-            if(currentLeader == null) Double.POSITIVE_INFINITY else curryiedCarFollowingModel(currentLeader)
-
-        val newLeader = targetLanePerceivedVehicles
-            .filter { it.obstacleRelativePosition.x >= 0 }.minByOrNull { it.obstacleRelativePosition.x }
-
-        val newLeaderRelatedAcceleration =
-            if(newLeader == null) Double.POSITIVE_INFINITY else curryiedCarFollowingModel(newLeader)
-
-        if(newLeaderRelatedAcceleration > currentLeaderRelatedAcceleration) {
-            // We can increase the speed in the new lane, let's check that we won't make the new follower brake
-            // by looking at the headway time (note: this could be changed to something else)
-            val newFollower = targetLanePerceivedVehicles
-                .filter { it.obstacleRelativePosition.x < 0 }.maxByOrNull { it.obstacleRelativePosition.x }
-            if(newFollower != null) {
-                val newFollowerDistance = abs(newFollower.obstacleRelativePosition.x)
-                val newFollowerVelocity = newFollower.obstacleRelativeVelocity.x + currentSpeed
-                val headway = newFollowerDistance / (newFollowerVelocity)
-
-                if(headway > 2.0) {
-                    return true
-                }
-            }
-        }
-    }
-
-    return false
 
 }

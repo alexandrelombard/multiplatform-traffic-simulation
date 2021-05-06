a57a3af4f8f89c710ecc3ef8e3914f43dd40b117
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-05-06 08:19:54 +0200

    Adding a test class for RT-ACC

---
+++ car-behavior/src/jvmTest/kotlin/fr/ciadlab/sim/car/behavior/longitudinal/ReactionTimeAdaptiveCruiseControlTest.kt
@@ -0,0 +1,44 @@
+package fr.ciadlab.sim.car.behavior.longitudinal
+
+import fr.ciadlab.sim.physics.Units.KilometersPerHour
+import fr.ciadlab.sim.physics.unit
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class ReactionTimeAdaptiveCruiseControlTest {
+
+    @Test
+    fun testBraking() {
+        val a = reactionTimeAdaptiveCruiseControl(
+            50.0 unit KilometersPerHour,
+            40.0 unit KilometersPerHour,
+            20.0)
+        assertTrue(a < 0)
+    }
+
+    @Test
+    fun testAcceleration() {
+        val a = reactionTimeAdaptiveCruiseControl(
+            50.0 unit KilometersPerHour,
+            80.0 unit KilometersPerHour,
+            30.0,
+            tau = 0.5)
+        assertTrue(a > 0)
+    }
+
+    @Test
+    fun testCompareTau() {
+        val aTau1 = reactionTimeAdaptiveCruiseControl(
+            50.0 unit KilometersPerHour,
+            80.0 unit KilometersPerHour,
+            30.0,
+            tau = 0.5)
+        val aTau2 = reactionTimeAdaptiveCruiseControl(
+            50.0 unit KilometersPerHour,
+            80.0 unit KilometersPerHour,
+            30.0,
+            tau = 2.0)
+        assertTrue(aTau1 > aTau2)
+    }
+
+}

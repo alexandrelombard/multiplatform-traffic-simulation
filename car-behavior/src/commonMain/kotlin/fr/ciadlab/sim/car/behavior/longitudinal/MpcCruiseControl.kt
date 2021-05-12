59ffa955798c358201dcce07ea04bec5fa45a86e
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-05-12 16:23:56 +0200

    Working on other longitudinal models

--- car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/longitudinal/MpcCruiseControl.kt
+++ car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/longitudinal/MpcCruiseControl.kt
@@ -1,5 +1,24 @@
 package fr.ciadlab.sim.car.behavior.longitudinal
 
-fun mpcCruiseControl(): Double {
+import kotlin.math.max
+import kotlin.math.min
+
+fun mpcCruiseControl(
+    velocity: Double,
+    targetVelocity: Double,
+    relativeVelocity: Double,
+    leaderMaximumDeceleration: Double,
+    intervehicularDistance: Double,
+    maximumAcceleration: Double,
+    headwayTime: Double,
+    tau: Double
+): Double {
+    // Compute command to reach target speed
+    val aTargetSpeed = min(maximumAcceleration, (targetVelocity - velocity) / tau)
+
+    // Compute command to maintain safe headway time
+    val leaderVelocity = velocity + relativeVelocity
+    val leaderVelocityModel = { t: Double -> max(0.0, leaderVelocity - leaderMaximumDeceleration * t) }
+
     TODO("Not yet implemented")
 }

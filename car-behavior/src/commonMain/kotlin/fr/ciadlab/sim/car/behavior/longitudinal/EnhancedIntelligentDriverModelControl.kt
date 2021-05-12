04daf31558b4cbda30f9e8de707012bda1a75352
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-05-12 11:45:44 +0200

    Working on other longitudinal models

--- car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/longitudinal/EnhancedIntelligentDriverModelControl.kt
+++ car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/longitudinal/EnhancedIntelligentDriverModelControl.kt
@@ -1,5 +1,8 @@
 package fr.ciadlab.sim.car.behavior.longitudinal
 
+import kotlin.math.pow
+import kotlin.math.tanh
+
 /**
  * Applies IDM+ to define the desired acceleration of a vehicle
  * @param intervehicularDistance the distance between the two vehicles (0.0 means the vehicle are colliding)
@@ -18,11 +21,25 @@ fun enhancedIntelligentDriverModelControl(
     velocity: Double,
     relativeVelocity: Double,
     desiredVelocity: Double,
+    leaderAcceleration: Double,
     maximumAcceleration: Double = 2.0,
     comfortableBrakingDeceleration: Double = 4.0,
     minimumSpacing: Double = 2.0,
     desiredHeadwayTime: Double = 2.0,
-    aggressivenessExponent: Double = 4.0): Double {
-    TODO("Not implemented")
+    aggressivenessExponent: Double = 4.0,
+    coolnessFactor: Double = 0.99): Double {
+    val vl = velocity + relativeVelocity
+    val aIdm = intelligentDriverModelControl(
+        intervehicularDistance, velocity, relativeVelocity, desiredVelocity, maximumAcceleration,
+        comfortableBrakingDeceleration, minimumSpacing, desiredHeadwayTime, aggressivenessExponent)
 
+    val theta = if(velocity - vl >= 0) 1.0 else 0.0
+    val aCah =
+        if(vl * (velocity - vl) <= -2.0 * intervehicularDistance * leaderAcceleration)
+            (velocity.pow(2.0) * leaderAcceleration) / (vl.pow(2.0) - 2.0 * intervehicularDistance * leaderAcceleration)
+        else
+            leaderAcceleration - ((velocity - vl).pow(2.0) * theta) / (2.0 * intervehicularDistance)
+
+
+    return if (aIdm >= aCah) aIdm else (1.0 - coolnessFactor) * aIdm + coolnessFactor * (aCah + comfortableBrakingDeceleration * tanh((aIdm - aCah) / comfortableBrakingDeceleration))
 }

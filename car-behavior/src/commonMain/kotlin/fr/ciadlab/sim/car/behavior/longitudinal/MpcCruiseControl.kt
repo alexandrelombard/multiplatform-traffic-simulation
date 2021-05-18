24bc5aea29b66e425c7e8ca5c0a8cc471ba6b3c0
Alexandre Lombard <alexandre.lombard@utbm.fr>
2021-05-18 14:02:32 +0200

    Working on other longitudinal models

--- car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/longitudinal/MpcCruiseControl.kt
+++ car-behavior/src/commonMain/kotlin/fr/ciadlab/sim/car/behavior/longitudinal/MpcCruiseControl.kt
@@ -1,24 +1,42 @@
 package fr.ciadlab.sim.car.behavior.longitudinal
 
-import kotlin.math.max
-import kotlin.math.min
+// Source: https://fr.mathworks.com/help/mpc/ug/adaptive-cruise-control-using-model-predictive-controller.html
 
 fun mpcCruiseControl(
-    velocity: Double,
-    targetVelocity: Double,
-    relativeVelocity: Double,
-    leaderMaximumDeceleration: Double,
-    intervehicularDistance: Double,
-    maximumAcceleration: Double,
-    headwayTime: Double,
-    tau: Double
+    velocity: Double,                       // v_ego
+    targetVelocity: Double,                 // v_set
+    relativeVelocity: Double,               // v_lead - v_ego
+    leaderMaximumDeceleration: Double,      // b_l
+    intervehicularDistance: Double,         // d_rel
+    minimumDeceleration: Double,            // a_min (ego)
+    maximumAcceleration: Double,            // a_max (both)
+    timeGap: Double,                        // t_gap
+    dDefault: Double,                       // Ddefault => minimum safe distance
+    tau: Double,                            // Ts
+    steps: Int
 ): Double {
-    // Compute command to reach target speed
-    val aTargetSpeed = min(maximumAcceleration, (targetVelocity - velocity) / tau)
+    val dSafe = { vEgo: Double -> dDefault + timeGap * vEgo}
+    val velocityModel = { acc: Double, dt: Double, v: Double -> v + acc * dt  }
 
-    // Compute command to maintain safe headway time
-    val leaderVelocity = velocity + relativeVelocity
-    val leaderVelocityModel = { t: Double -> max(0.0, leaderVelocity - leaderMaximumDeceleration * t) }
+
 
     TODO("Not yet implemented")
+}
+
+fun mpc() {
+    var vEgo = 0.0
+    var xEgo = 0.0
+    var vLead = 0.0
+    var xLead = 0.0
+
+    var acc = 0.0
+    var dt = 0.1
+
+    vEgo += acc * dt
+    xEgo += vEgo * dt
+    xLead += vLead * dt
+
+    var dRel = xLead - xEgo
+
+    TODO("Not yet implement")
 }
